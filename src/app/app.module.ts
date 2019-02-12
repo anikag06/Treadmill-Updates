75e16a64b5279ef388ad355e7ab9ef81bf7063eb
arkaghosh024 <arkaghosh024@gmail.com>
2019-02-12 20:42:02 +0530

    PWA - almost done, need to intercept the install prompt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from '@/app-routing.module';
 import { AppComponent } from '@/app.component';
@@ -12,6 +13,7 @@ import { LoggerService } from '@/shared/logger.service';
 import { PreLoginFooterComponent } from '@/shared/pre-login/pre-login-footer/pre-login-footer.component';
 import { MatContactUsDialogComponent } from '@/shared/pre-login/mat-contact-us-dialog/mat-contact-us-dialog.component';
 import { DialogSize } from '@/shared/dialog-size.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import { DialogSize } from '@/shared/dialog-size.service';
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [LoginComponent, LoggerService, DialogSize],
   bootstrap: [AppComponent],
