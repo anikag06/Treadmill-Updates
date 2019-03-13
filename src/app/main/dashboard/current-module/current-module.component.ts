a77a0419d99ae550b7cd5fa449d58a5fafac1dc5
Sourav Moitra <sourav.moitr@gmail.com>
2019-03-13 18:41:43 +0530

    Dashboard UI complete

--- src/app/main/dashboard/current-module/current-module.component.ts
+++ src/app/main/dashboard/current-module/current-module.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModulesService } from '@/main/modules/modules.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Module } from '@/main/modules/module.model';
 import { map } from 'rxjs/operators';
 import { ACTIVE } from '@/app.constants';
+import { Category } from '@/main/shared/category.model';
+import { CategoryService } from '@/main/shared/category.service';
 
 @Component({
   selector: 'app-current-module',
   templateUrl: './current-module.component.html',
   styleUrls: ['./current-module.component.scss'],
-  providers: [ModulesService]
+  providers: [ModulesService, CategoryService]
 })
-export class CurrentModuleComponent implements OnInit {
+export class CurrentModuleComponent implements OnInit, OnDestroy {
 
   module$!: Observable<Module>;
+  categories$!: Observable<Category[]>;
+  categorySubscription!: Subscription;
 
   constructor(
-    private modulesService: ModulesService
+    private modulesService: ModulesService,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
@@ -24,6 +29,14 @@ export class CurrentModuleComponent implements OnInit {
       .pipe(
         map(modules => modules.find(module => module.status === ACTIVE))
       );
+    this.categorySubscription = this.module$
+        .subscribe((module) => {
+            this.categories$ = this.categoryService.getCategories(module.name);
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.categorySubscription.unsubscribe();
   }
 
 }
