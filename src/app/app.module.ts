0124e620f990ad729c41c8fba131672e7a2c3225
Sourav Moitra <sourav.moitr@gmail.com>
2019-03-16 17:48:03 +0530

    Major moves

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -15,34 +15,13 @@ import { MatContactUsDialogComponent } from '@/shared/mat-contact-us-dialog/mat-
 import { DialogSize } from '@/shared/dialog-size.service';
 import { environment } from '../environments/environment';
 import { LandingPageComponent } from '@/pre-login/landing-page/landing-page.component';
-import { ModulesComponent } from '@/main/modules/modules.component';
-import { DashboardComponent } from '@/main/dashboard/dashboard.component';
 import { PreLoginComponent } from '@/pre-login/pre-login.component';
 import { MatContactUsDialogService } from '@/shared/mat-contact-us-dialog/mat-contact-us-dialog.service';
-import { ModuleDetailComponent } from '@/main/modules/module-detail/module-detail.component';
-import { ModuleItemComponent } from '@/main/modules/module-list/module-item/module-item.component';
-import { ModuleOverviewComponent } from '@/main/modules/module-list/module-overview/module-overview.component';
-import { ModuleListComponent } from '@/main/modules/module-list/module-list.component';
-import { CategorySmallComponent } from '@/main/modules/module-list/module-overview/category-small/category-small.component';
-import { LocalStorageService } from '@/shared/localstorage.service';
-import { NotFoundComponent } from '@/shared/not-found/not-found.component';
-import { CategoryBigComponent } from './main/modules/module-detail/category-big/category-big.component';
-import { SectionCardComponent } from '@/main/modules/module-detail/section-card/section-card.component';
 import { AuthService } from './shared/auth/auth.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthModule } from './auth.module';
-import { SideNavComponent } from '@/main/shared/side-nav/side-nav.component';
-import { LogoutComponent } from './shared/auth/logout/logout.component';
-import { ProgressComponent } from './main/dashboard/progress/progress.component';
-import { CurrentModuleComponent } from './main/dashboard/current-module/current-module.component';
-import { ThingsTodoComponent } from './main/dashboard/things-todo/things-todo.component';
-import { NotificationsSideComponent } from './main/dashboard/notifications-side/notifications-side.component';
-import { TimeAgoPipe } from './shared/time-ago.pipe';
-import { ChatbotAvatarComponent } from './main/dashboard/chatbot-avatar/chatbot-avatar.component';
-import { GamesComponent } from './main/games/games.component';
-import { GamesListComponent } from './main/games/games-list/games-list.component';
-import { GameItemComponent } from './main/games/games-list/game-item/game-item.component';
 
+import { AuthModule } from './auth.module';
+import { MainModule } from '@/main/main.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,28 +30,7 @@ import { GameItemComponent } from './main/games/games-list/game-item/game-item.c
     PreLoginFooterComponent,
     MatContactUsDialogComponent,
     LandingPageComponent,
-    ModulesComponent,
-    DashboardComponent,
-    PreLoginComponent,
-    ModuleDetailComponent,
-    ModuleItemComponent,
-    ModuleOverviewComponent,
-    ModuleListComponent,
-    CategorySmallComponent,
-    NotFoundComponent,
-    CategoryBigComponent,
-    SectionCardComponent,
-    SideNavComponent,
-    LogoutComponent,
-    ProgressComponent,
-    CurrentModuleComponent,
-    ThingsTodoComponent,
-    NotificationsSideComponent,
-    TimeAgoPipe,
-    ChatbotAvatarComponent,
-    GamesComponent,
-    GamesListComponent,
-    GameItemComponent,
+    PreLoginComponent
   ],
   imports: [
     BrowserModule,
@@ -84,6 +42,7 @@ import { GameItemComponent } from './main/games/games-list/game-item/game-item.c
     AppRoutingModule,
     HttpClientModule,
     AuthModule,
+    MainModule,
     ServiceWorkerModule.register('sw-master.js', { enabled: environment.production })
   ],
   providers: [
@@ -91,11 +50,9 @@ import { GameItemComponent } from './main/games/games-list/game-item/game-item.c
     LoggerService,
     DialogSize,
     MatContactUsDialogService,
-    LocalStorageService,
     AuthService
   ],
   bootstrap: [AppComponent],
   entryComponents: [MatLoginDialogComponent, MatContactUsDialogComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,59 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModulesComponent } from '@/main/modules/modules.component';
+import { DashboardComponent } from '@/main/dashboard/dashboard.component';
+import { ModuleDetailComponent } from './modules/module-detail/module-detail.component';
+import { ModuleItemComponent } from './modules/module-list/module-item/module-item.component';
+import { ModuleOverviewComponent } from './modules/module-list/module-overview/module-overview.component';
+import { ModuleListComponent } from './modules/module-list/module-list.component';
+import { CategorySmallComponent } from './shared/category-small/category-small.component';
+import { LocalStorageService } from '../shared/localstorage.service';
+import { NotFoundComponent } from '../shared/not-found/not-found.component';
+import { CategoryBigComponent } from './modules/module-detail/category-big/category-big.component';
+import { SectionCardComponent } from './modules/module-detail/section-card/section-card.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { LogoutComponent } from '../shared/auth/logout/logout.component';
+import { ProgressComponent } from './dashboard/progress/progress.component';
+import { CurrentModuleComponent } from './dashboard/current-module/current-module.component';
+import { ThingsTodoComponent } from './dashboard/things-todo/things-todo.component';
+import { NotificationsSideComponent } from './dashboard/notifications-side/notifications-side.component';
+import { TimeAgoPipe } from '../shared/time-ago.pipe';
+import { ChatbotAvatarComponent } from './dashboard/chatbot-avatar/chatbot-avatar.component';
+import { GamesComponent } from './games/games.component';
+import { GamesListComponent } from './games/games-list/games-list.component';
+import { GameItemComponent } from './games/games-list/game-item/game-item.component';
+
+@NgModule({
+  declarations: [
+    ModulesComponent,
+    DashboardComponent,
+    ModuleDetailComponent,
+    ModuleItemComponent,
+    ModuleOverviewComponent,
+    ModuleListComponent,
+    CategorySmallComponent,
+    NotFoundComponent,
+    CategoryBigComponent,
+    SectionCardComponent,
+    SideNavComponent,
+    LogoutComponent,
+    ProgressComponent,
+    CurrentModuleComponent,
+    ThingsTodoComponent,
+    NotificationsSideComponent,
+    TimeAgoPipe,
+    ChatbotAvatarComponent,
+    GamesComponent,
+    GamesListComponent,
+    GameItemComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    LocalStorageService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MainModule { }
