e0741ca7a191d79cc2e8b0dd9dd7d42e0ab865cd
Aviral Verma <aviralverma.verma@gmail.com>
2019-04-05 12:42:31 +0530

    removed redundancy in code and minor bug

--- src/app/main/side-nav/side-nav.component.ts
+++ src/app/main/side-nav/side-nav.component.ts
@@ -14,7 +14,18 @@ export class SideNavComponent {
     .pipe(
       map(result => result.matches)
     );
-    items = ['Dashboard', 'Modules', 'Games', 'Groups', 'Scores', 'Resources', 'Settings', '', 'Need to talk', 'Logout']
+    items1 = ['Dashboard', 'Modules', 'Games', 'Groups', 'Scores', 'Resources', 'Settings'];
+    
+    items2 = ['Need to talk', 'Logout'];
+
+    links1 = ['dashboard', 'modules', 'games', '', '', '', ''];
+
+    links2 = ['', ''];
+
+    icons1 = ['dashboard.svg', 'modules.svg', 'games.svg', 'groups.svg', 'scores.svg', 'resources.svg', 'settings.svg'];
+
+    icons2 = ['need to talk.svg', 'logout.svg']
+
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
