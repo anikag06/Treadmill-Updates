29c7cbe66f83e7c7e667b1fb846717b0d8b7c002
Arka <arkaghosh024@gmail.com>
2019-11-21 17:06:18 +0530

    mi game - get situations from backend
    get situations from backend

--- src/app/main/games/games-list/common-game/mental-imagery/scenario.model.ts
+++ src/app/main/games/games-list/common-game/mental-imagery/scenario.model.ts
@@ -2,7 +2,7 @@ export class Scenario {
     constructor(
         public problemBeforeDash: string,
         public problemAfterDash: string,
-        public points: number,
+        // public points: number,
         public scenarioNext: Scenario | null,
         public wrongText: string,
         public correctText: string | null
