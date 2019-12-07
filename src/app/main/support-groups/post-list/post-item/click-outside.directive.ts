89a1423089931cc0a0d51ac957edb781b1a74c5b
Shiv <treadwill.iitk.2@gmail.com>
2019-12-07 13:41:47 +0530

    code refactoring

--- src/app/main/support-groups/post-list/post-item/click-outside.directive.ts
+++ src/app/main/support-groups/post-list/post-item/click-outside.directive.ts
@@ -1,28 +1,29 @@
-import {Directive, ElementRef, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
-import {fromEvent} from "rxjs";
-import {delay, tap} from 'rxjs/operators';
+import { Directive, ElementRef, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { fromEvent, Subscription, Observable } from "rxjs";
+import { delay, tap } from 'rxjs/operators';
 @Directive({
   selector: '[click-outside]'
 })
 export class ClickOutsideDirective implements OnInit {
   private listening: boolean;
-  private globalClick: any;
+  private globalClick!: Observable<MouseEvent>;
+  private globalClickSubscription!: Subscription;
   @Output('clickOutside') clickOutside: EventEmitter<Object>;
   constructor(private _elRef: ElementRef) {
     this.listening = false;
     this.clickOutside = new EventEmitter();
   }
   ngOnInit() {
-    this.globalClick = fromEvent(document, 'click')
-    this.globalClick.pipe(delay(1), tap(() => {
+    this.globalClick = fromEvent<MouseEvent>(document, 'click')
+    this.globalClickSubscription = this.globalClick.pipe(delay(1), tap(() => {
       this.listening = true;
     })).subscribe(((event: MouseEvent) => {
       this.onGlobalClick(event);
     }))
   }
-  // ngOnDestroy() {
-  //   this.globalClick.();
-  // }
+  ngOnDestroy() {
+    this.globalClickSubscription.unsubscribe();
+  }
   onGlobalClick(event: MouseEvent) {
     if (event instanceof MouseEvent && this.listening === true) {
       if (this.isDescendant(this._elRef.nativeElement, event.target) === true) {
@@ -38,7 +39,7 @@ export class ClickOutsideDirective implements OnInit {
       }
     }
   }
-  isDescendant(parent: any, child:any) {
+  isDescendant(parent: any, child: any) {
     let node = child;
     while (node !== null) {
       if (node === parent) {
