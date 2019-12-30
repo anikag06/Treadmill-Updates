ed9c1a0e141e5e5f8d19e31e27aadf09abea3d42
td_1 <treadwill.iitk.1@gmail.com>
2019-12-30 19:24:42 +0530

    modify beliefs

---
+++ src/app/main/resources/forms/worry-productively-form/techniques/modify-beliefs/modify-beliefs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-beliefs',
+  templateUrl: './modify-beliefs.component.html',
+  styleUrls: ['./modify-beliefs.component.scss']
+})
+export class ModifyBeliefsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
