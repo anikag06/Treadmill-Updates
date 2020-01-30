279f7f67545c0a92129499444e73e82eda6450e4
sradha <sradhab@iitk.ac.in>
2020-01-30 15:11:29 +0530

    HTML page of resources content, next button
    some styling changes

--- src/app/main/resources/control-content/control-content.component.ts
+++ src/app/main/resources/control-content/control-content.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FlowService } from '@/main/flow/flow.service';
 import { map, switchMap } from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Route, Router} from '@angular/router';
 import { StepsDataService } from '@/main/resources/shared/steps-data.service';
 import {LoadFilesService} from '@/main/games/shared/load-files.service';
+import {ControlContentService} from '@/main/resources/control-content/control-content.service';
+import {FlowStepNavigationService} from '@/main/shared/flow-step-navigation.service';
+import {PassDataService} from '@/main/resources/conversation-group/passdata.service';
+import {StepCompleteData} from '@/main/resources/shared/completion-data.model';
 
 @Component({
   selector: 'app-control-content',
@@ -11,16 +15,29 @@ import {LoadFilesService} from '@/main/games/shared/load-files.service';
   styleUrls: ['./control-content.component.scss'],
   providers: [FlowService],
 })
-export class ControlContentComponent implements OnInit {
+export class ControlContentComponent implements OnInit{
   // x = '<div class="row"><div class="col"><h2 >hi</h2></div></div>' ;
   displayHtml: string | undefined;
+  displayHtmlHeader: string | undefined;
+  next_step_id!: number;
+  completionData: StepCompleteData = new StepCompleteData(0, 0);
+  current_step_id!: number;
+  isLastStep =  false;
+
+
 
   constructor(
     private flowService: FlowService,
     private activateRoute: ActivatedRoute,
     private stepDataService: StepsDataService,
-    private loadFilesService: LoadFilesService
+    private loadFilesService: LoadFilesService,
+    private controlContentService: ControlContentService,
+    private flowStepService: FlowStepNavigationService,
+    private router: Router,
+    private passData: PassDataService
   ) {}
+  nextBtnShow = false;
+
 
   ngOnInit() {
     this.loadFilesService.loadExternalStyles('/control-content-styles.css').then(() => {}).catch(() => {});
@@ -33,8 +50,57 @@ export class ControlContentComponent implements OnInit {
       .subscribe((control_data: any) => {
         console.log('control data: ', control_data);
         this.displayHtml = control_data.data.step_data.data.html_content;
+        this.displayHtmlHeader = control_data.data.step_data.data.title;
+        this.next_step_id = control_data.data.next_step_id;
+        this.current_step_id = control_data.data.id;
+        this.isLastStep = control_data.data.is_last_step;
+        if (control_data.data.status === 'COMPLETED') {
+          this.nextBtnShow = true;
+        }
+
       });
   }
 
+  onHtmlNext() {
+    this.flowStepService.getNextStepData(this.next_step_id).subscribe(next_step => {
+      console.log('next_step_detail is', next_step);
+      console.log('next_step_data is', next_step.data);
+      const next_step_url = this.flowStepService.goToFlowNextStep(next_step.data);
+      console.log('url is', next_step_url);
+      this.router.navigate([next_step_url]);
+    });
+
+  }
+
+
+
+  onHtmlComplete() {
+    this.completionData.step_id = this.current_step_id;
+    this.completionData.time_spent = 100;
+
+    this.stepDataService.storeCompletionData(this.completionData)
+      .subscribe(() => {});
+
+
+    this.nextBtnShow = true;
+  }
+
+
+
+  onHtmlDashboard() {
+    this.router.navigate(['/']);
+  }
+
+  onHtmlNextClick(){
+    this.router.navigate([this.onHtmlNext()]);
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
 
 }
+
+
+
+
+
+
+
---
+++ src/app/main/resources/control-content/control-content.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ControlContentService{
+  constructor() {
+  }
+  getHtml(){
+   // ControlC
+
+  }
+}
