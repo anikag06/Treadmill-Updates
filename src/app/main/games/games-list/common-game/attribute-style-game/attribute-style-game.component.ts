4fbe2472bb865b0481b1e69541f37957de68e6bd
arkaghosh <arkaghosh024@gmail.com>
2020-05-09 19:15:25 +0530

    Fixed minor issues

--- src/app/main/games/games-list/common-game/attribute-style-game/attribute-style-game.component.ts
+++ src/app/main/games/games-list/common-game/attribute-style-game/attribute-style-game.component.ts
@@ -1,15 +1,20 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { LoadFilesService } from '@/main/games/shared/load-files.service';
 import { GamesAuthService } from '@/main/games/shared/games-auth.service';
 import { GamePlayService } from '@/main/games/shared/game-play.service';
-import {GamesFeedbackComponent} from '@/main/games/games-list/common-game/games-feedback/games-feedback.component';
+import { GamesFeedbackComponent } from '@/main/games/games-list/common-game/games-feedback/games-feedback.component';
 import { DialogBoxService } from '@/main/shared/custom-dialog/dialog-box.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import {map, switchMap} from "rxjs/operators";
-import {FlowService} from "@/main/flow/flow.service";
-import {ActivatedRoute} from "@angular/router";
-import {StepsDataService} from "@/main/resources/shared/steps-data.service";
-
+import { map, switchMap } from 'rxjs/operators';
+import { FlowService } from '@/main/flow/flow.service';
+import { StepsDataService } from '@/main/resources/shared/steps-data.service';
 
 declare var ASGAnswer: any;
 
@@ -18,8 +23,7 @@ declare var ASGAnswer: any;
   templateUrl: './attribute-style-game.component.html',
   styleUrls: ['./attribute-style-game.component.scss'],
 })
-export class AttributeStyleGameComponent implements OnInit {
-
+export class AttributeStyleGameComponent implements OnInit, OnDestroy {
   navbarTitle!: string;
   stepGroupSequence!: number;
   stepSequence!: number;
@@ -36,7 +40,6 @@ export class AttributeStyleGameComponent implements OnInit {
   ) {}
   @ViewChild('newElement', { static: false }) element!: ElementRef;
 
-
   ngOnInit() {
     this.loadFileService
       .loadExternalScript('./assets/games/Attribution-style-game/src/app.js')
@@ -45,26 +48,24 @@ export class AttributeStyleGameComponent implements OnInit {
     this.activatedRoute.params
       .pipe(
         map(v => v.id),
-        switchMap(id =>  this.stepDataService
-          .getStepData(id)),
+        switchMap(id => this.stepDataService.getStepData(id)),
       )
-      .subscribe(
-        (res: any) => {
-          const step = res.data;
-          console.log('RESPONSE', res.data, step.status);
-          // for navbar title
-          this.stepGroupSequence = step.step_group_sequence + 1;
-          this.stepSequence = step.sequence + 1;
-          this.stepName = step.name;
-          this.navbarTitle =
-            this.stepGroupSequence.toString() +
-            '.' +
-            this.stepSequence.toString() +
-            ' ' +
-            this.stepName;
-          console.log('STEP DETAIL:', this.navbarTitle);
-          this.flowService.stepDetail.emit(this.navbarTitle);
-        } );
+      .subscribe((res: any) => {
+        const step = res.data;
+        console.log('RESPONSE', res.data, step.status);
+        // for navbar title
+        this.stepGroupSequence = step.step_group_sequence + 1;
+        this.stepSequence = step.sequence + 1;
+        this.stepName = step.name;
+        this.navbarTitle =
+          this.stepGroupSequence.toString() +
+          '.' +
+          this.stepSequence.toString() +
+          ' ' +
+          this.stepName;
+        console.log('STEP DETAIL:', this.navbarTitle);
+        this.flowService.stepDetail.emit(this.navbarTitle);
+      });
     this.gamePlayService.ASGUserData();
 
     /* this.gamesAuthService.atGetAnswers().subscribe(
@@ -74,9 +75,6 @@ export class AttributeStyleGameComponent implements OnInit {
       }
     );*/
   }
-
-
-
 
   @HostListener('window:ASGPostIndividualLevelPerformance')
   postIndividualLevelPerformance() {
@@ -104,9 +102,9 @@ export class AttributeStyleGameComponent implements OnInit {
 
   @HostListener('window:Feedback')
   openFeedbackPopup() {
-      this.dialogBoxService.setDialogChild(GamesFeedbackComponent);
-      const domEvent = new CustomEvent('overlayCalledEvent', { bubbles: true });
-      this.element.nativeElement.dispatchEvent(domEvent);
+    this.dialogBoxService.setDialogChild(GamesFeedbackComponent);
+    const domEvent = new CustomEvent('overlayCalledEvent', { bubbles: true });
+    this.element.nativeElement.dispatchEvent(domEvent);
   }
 
   @HostListener('window:ASGPut')
@@ -118,13 +116,10 @@ export class AttributeStyleGameComponent implements OnInit {
   ASGgoHome() {
     console.log('gohome');
     this.router.navigate(['/']);
-
-
   }
 
   ngOnDestroy() {
     console.log('exec- ng on destroy');
     this.gamePlayService.closeASGame();
   }
-
 }
