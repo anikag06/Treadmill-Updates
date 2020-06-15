96ed740e4bdb7841880d3defe1631d32317d7d30
Shiv <treadwill.iitk.2@gmail.com>
2020-06-15 10:34:30 +0530

    widgets position bugfix

--- src/app/main/shared/mood-tracker/mood-tracker.service.ts
+++ src/app/main/shared/mood-tracker/mood-tracker.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { FEELING_LIST_API } from '@/app.constants';
+import {AuthService} from '@/shared/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoodTrackerService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getFeelingsList() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.authService.getToken(),
+      }),
+    };
     return this.http
-      .get(environment.API_ENDPOINT + FEELING_LIST_API)
+      .get(environment.API_ENDPOINT + FEELING_LIST_API, httpOptions)
       .toPromise();
   }
 }
