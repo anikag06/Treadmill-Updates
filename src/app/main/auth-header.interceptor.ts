8750aea64de5edec8c6d6fbc759e60e4a64ccaaa
latika <lagarwal@iitk.ac.in>
2020-07-02 17:43:55 +0530

    Interceptor for headers added

---
+++ src/app/main/auth-header.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import {AuthService} from "@/shared/auth/auth.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+
+export class AuthHeaderInterceptor implements HttpInterceptor {
+  constructor( private authService: AuthService) {
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Clone the request to add the new header
+    const clonedRequest = req.clone({ setHeaders: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.authService.getToken(),
+    }
+    });
+
+    // Pass the cloned request instead of the original request to the next handle
+    return next.handle(clonedRequest);
+  }
+}
