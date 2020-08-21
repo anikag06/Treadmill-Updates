ed8a5f47b6b449badecf7eb33339cab211e9b7cb
arkaghosh <arkaghosh024@gmail.com>
2020-08-21 10:51:32 +0530

    part of previous commit

--- src/app/main/shared/game.model.ts
+++ src/app/main/shared/game.model.ts
@@ -1,15 +1,13 @@
 import slugify from 'slugify';
 
 export class Game {
-    public name: string;
-    public thumbnail: string;
-    public description: string;
-    public slug: string;
+  public name: string;
+  public thumbnail: string;
+  public slug: string;
 
-    constructor(name: string, thumbnail: string, description: string, slug?: string) {
-        this.name = name;
-        this.thumbnail = thumbnail;
-        this.description = description;
-        this.slug = slug || slugify(name, {lower: true});
-    }
+  constructor(name: string, thumbnail: string, slug?: string) {
+    this.name = name;
+    this.thumbnail = thumbnail;
+    this.slug = slug || slugify(name, { lower: true });
+  }
 }
