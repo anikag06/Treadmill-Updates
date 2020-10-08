8fd120eb9c208d5d3a30139e39d86a2962e87e92
latika <lagarwal@iitk.ac.in>
2020-10-08 09:50:23 +0530

    Loading gif added

--- src/app/main/resources/forms/forms-list/forms-list.component.ts
+++ src/app/main/resources/forms/forms-list/forms-list.component.ts
@@ -14,6 +14,7 @@ import { User } from '@/shared/user.model';
 })
 export class FormsListComponent implements OnInit {
   user!: User;
+  showLoading = true;
 
   constructor(
     private formsService: FormsService,
@@ -46,5 +47,10 @@ export class FormsListComponent implements OnInit {
       });
     }
     this.router.navigateByUrl(form.path);
+  }
+  removeLoading() {
+    setTimeout( () => {
+      this.showLoading = false;
+    }, 100);
   }
 }
