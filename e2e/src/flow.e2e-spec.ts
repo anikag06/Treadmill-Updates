35e97a7937b1d960034ebe5548762df3a194833f
latika <lagarwal@iitk.ac.in>
2020-10-22 17:06:34 +0530

    e2e changes

--- e2e/src/flow.e2e-spec.ts
+++ e2e/src/flow.e2e-spec.ts
@@ -197,14 +197,14 @@ describe('treadwill Flow Experimental Group', () => {
       fp.findProgressElement('Solve my problem form');
       fp.reload();
       fp.clickGoto();
+      fp.findProgressElement('Help us improve');
+      fp.fillSurveyForm();
+      browser.sleep(3000);
       if (testfor === 'dropout' && moduleNum === 3) {
         fp.navigateToDashboard();
         console.log('CHECK FOR DROPOUT  AT MODULE 3');
         fp.checkForDropout(loginTime);
       }
-      fp.findProgressElement('Help us improve');
-      fp.fillSurveyForm();
-      browser.sleep(2000);
       fp.findProgressElement('Finish module');
       fp.evaluateMood();
       page.findPhq();
