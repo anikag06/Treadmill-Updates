0ee071c2a7316b0bd726ee3315beb235d9cd1156
Shiv <treadwill.iitk.2@gmail.com>
2020-11-12 20:06:46 +0530

    forms ui fixes user profile ui changes landing page ui fixes

--- src/app/main/shared/user-profile/user-profile.component.ts
+++ src/app/main/shared/user-profile/user-profile.component.ts
@@ -20,32 +20,35 @@ export class UserProfileComponent implements OnInit, AfterViewInit {
   constructor(
     private element: ElementRef,
     private userProfileService: UserProfileService,
-    private authService: AuthService,
+    private authService: AuthService
   ) {}
   @Input() userProfile!: UserProfile;
   showLoading = true;
   profileLoaded = false;
   user!: User;
+  goldBadgesColor = '#D4AF37';
+  silverBadgesColor = '#96959A';
+  bronzeBadgesColor = '#CD7F32';
   ngOnInit() {}
 
   ngAfterViewInit() {
     const inkBar = this.element.nativeElement.querySelectorAll(
-      '.mat-tab-group.mat-primary .mat-ink-bar,.mat-tab-nav-bar.mat-primary .mat-ink-bar',
+      '.mat-tab-group.mat-primary .mat-ink-bar,.mat-tab-nav-bar.mat-primary .mat-ink-bar'
     );
     inkBar[0].setAttribute('style', 'background: black;');
     const tabLabel = this.element.nativeElement.querySelectorAll(
-      '.mat-tab-label',
+      '.mat-tab-label'
     );
     for (let i = 0; i < tabLabel.length; i++) {
       tabLabel[i].setAttribute(
         'style',
-        'min-width: 80px;height:40px;opacity:1',
+        'min-width: 80px;height:40px;opacity:1'
       );
     }
 
     if (window.innerWidth < MOBILE_WIDTH) {
       const headerText = this.element.nativeElement.querySelectorAll(
-        '.mat-card-header-text',
+        '.mat-card-header-text'
       );
       headerText[0].setAttribute('style', 'margin: 0px');
     }
