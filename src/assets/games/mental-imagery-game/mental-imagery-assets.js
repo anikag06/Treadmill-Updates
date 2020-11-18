ee7cc8456bc6191d74b1ccd36c73964563ff91ce
latika <lagarwal@iitk.ac.in>
2020-11-18 12:23:31 +0530

    MI game preload assets

---
+++ src/assets/games/mental-imagery-game/mental-imagery-assets.js
@@ -0,0 +1,15 @@
+if (document.images) {
+  img1 = new Image();
+  img2 = new Image();
+  img3 = new Image();
+  img4 = new Image();
+  img5 = new Image();
+
+
+  img1.src = "assets/games/mental-imagery-game/Background with texture.png";
+  img2.src = "assets/games/mental-imagery-game/How to play.svg";
+  img3.src = "assets/games/mental-imagery-game/Great.svg";
+  img4.src = "assets/games/mental-imagery-game/Popup with texture.png";
+  img5.src = "assets/games/mental-imagery-game/daydream background.png";
+
+}
