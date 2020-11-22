a02e82b77d5823b588731fac065a6e3e467db41e
latika <lagarwal@iitk.ac.in>
2020-11-22 16:09:08 +0530

    idc game preloaded svg assets

--- src/assets/games/cognitive-distortion-game/images/idc_preload_images.js
+++ src/assets/games/cognitive-distortion-game/images/idc_preload_images.js
@@ -4,12 +4,25 @@ if (document.images) {
   img3 = new Image();
   img4 = new Image();
   img5 = new Image();
+  img6 = new Image();
+  img7 = new Image();
+  img8 = new Image();
+  img9 = new Image();
+  img10 = new Image();
+  img11 = new Image();
+  img12 = new Image();
 
 
   img1.src = "assets/games/cognitive-distortion-game/Cognitive distortion game background.png";
-  img2.src = "assets/games/cognitive-distortion-game/Great.png";
-  img3.src = "assets/games/cognitive-distortion-game/play again.png";
-  img4.src = "assets/games/cognitive-distortion-game/replay.png";
-  img5.src = "assets/games/cognitive-distortion-game/time.png";
-
+  img2.src = "assets/games/cognitive-distortion-game/Great.svg";
+  img3.src = "assets/games/time.svg";
+  img4.src = "assets/games/cognitive-distortion-game/how to play graphic.svg";
+  img5.src = "assets/games/cognitive-distortion-game/clock icon.svg";
+  img6.src = "assets/games/cognitive-distortion-game/Gold badge symbol.svg";
+  img7.src = "assets/games/cognitive-distortion-game/Silver badge symbol.svg";
+  img8.src = "assets/games/cognitive-distortion-game/Bronze badge symbol.svg";
+  img9.src = "assets/games/cognitive-distortion-game/0 sec.svg";
+  img10.src = "assets/games/cognitive-distortion-game/Thinking errors.svg";
+  img11.src = "assets/games/cognitive-distortion-game/info.svg";
+  img12.src = "assets/games/replay.svg";
 }
