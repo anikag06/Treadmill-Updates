650605b948f6b461f6455ab8dd58b75e796a91b9
latika <lagarwal@iitk.ac.in>
2020-11-25 15:41:37 +0530

    slides preload assets svg

--- src/assets/slides/slides-assets.js
+++ src/assets/slides/slides-assets.js
@@ -3,10 +3,14 @@ if (document.images) {
   img2 = new Image();
   img3 = new Image();
   img4 = new Image();
+  img5 = new Image();
 
 
-  img1.src = "/assets/shared/Like_slide.png";
-  img2.src = "/assets/shared/Like_slide_selected.png";
-  img3.src = "/assets/shared/dislike_slide.png";
-  img4.src = "/assets/shared/dislike_slide_selected.png";
+
+  img1.src = "/assets/slides/Like_slide.svg";
+  img2.src = "/assets/slides/Like_slide_selected.svg";
+  img3.src = "/assets/slides/dislike_slide.svg";
+  img4.src = "/assets/slides/dislike_slide_selected.svg";
+  img5.src = "/assets/slides/meditation.svg";
+
 }
