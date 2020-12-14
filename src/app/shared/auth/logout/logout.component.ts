2d9a74261a70114fa908b88b9b07942b469ef856
latika <lagarwal@iitk.ac.in>
2020-12-14 16:11:41 +0530

    set title for logout

--- src/app/shared/auth/logout/logout.component.ts
+++ src/app/shared/auth/logout/logout.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import {Title} from "@angular/platform-browser";
+import {TREADWILL} from "@/app.constants";
 
 @Component({
   selector: 'app-logout',
@@ -8,7 +10,10 @@ import { AuthService } from '../auth.service';
   styles: [''],
 })
 export class LogoutComponent {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private titleService: Title,
+  ) {
     this.authService.logout(true);
+    this.titleService.setTitle(TREADWILL);
   }
 }
