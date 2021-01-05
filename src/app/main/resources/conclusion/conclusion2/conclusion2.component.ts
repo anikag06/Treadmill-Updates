adc07640675e57ea3c2fa3861a9c5a46d2d2d510
latika <lagarwal@iitk.ac.in>
2021-01-05 17:02:33 +0530

    conclusion page scrolled bug fix

--- src/app/main/resources/conclusion/conclusion2/conclusion2.component.ts
+++ src/app/main/resources/conclusion/conclusion2/conclusion2.component.ts
@@ -54,7 +54,8 @@ export class Conclusion2Component implements OnInit, OnDestroy {
   moodEvaluate!: boolean;
   showLoading = false;
 
-  @ViewChild('target', { static: false }) target!: ElementRef;
+  @ViewChild('target1', { static: false }) target1!: ElementRef;
+  @ViewChild('target2', { static: false }) target2!: ElementRef;
 
   constructor(
     private conclusionService: ConclusionService,
@@ -126,6 +127,7 @@ export class Conclusion2Component implements OnInit, OnDestroy {
             }
           });
       });
+    this.scrollup();
     this.quizService.questionnaire_active.subscribe((value: boolean) => {
       console.log('EVENT EMITTED', value);
       if (!value) {
@@ -174,7 +176,12 @@ export class Conclusion2Component implements OnInit, OnDestroy {
   }
   scrollDown() {
     setTimeout(() => {
-      this.target.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      this.target2.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }, 100);
+  }
+  scrollup() {
+    setTimeout(() => {
+      this.target1.nativeElement.scrollIntoView({ behavior: 'smooth' });
     }, 100);
   }
 }
