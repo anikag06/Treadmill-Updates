f4b241ef29f5bedf0664acdb5fcf6729076acfaf
Shiv <treadwill.iitk.2@gmail.com>
2021-02-03 15:08:36 +0530

    syntax error handler added in index file dead code removed from global error handler

--- src/app/shared/global-error-handler.ts
+++ src/app/shared/global-error-handler.ts
@@ -5,12 +5,7 @@ export class GlobalErrorHandler extends ErrorHandler {
   handleError(error: any): void {
     const chunkFailedMessage = /Loading chunk [\d]+ failed/;
 
-    const syntaxErrorMessage = /Uncaught SyntaxError: Unexpected token [\d]/;
-
-    if (
-      chunkFailedMessage.test(error.message) ||
-      syntaxErrorMessage.test(error.message)
-    ) {
+    if (chunkFailedMessage.test(error.message)) {
       window.location.reload();
     } else {
       super.handleError(error);
