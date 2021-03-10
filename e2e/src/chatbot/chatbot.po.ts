f61a0cfa3725efbbb69bf0ffd2021aeb284d5d62
latika <lagarwal@iitk.ac.in>
2021-03-10 14:58:50 +0530

    changes for testing

---
+++ e2e/src/chatbot/chatbot.po.ts
@@ -0,0 +1,19 @@
+/* tslint:disable:no-trailing-whitespace */
+import { browser, by, element, protractor } from 'protractor';
+import { AppPage } from '../app.po';
+
+export class ChatbotPage {
+  EC = protractor.ExpectedConditions;
+  firstLoginTime!: number;
+  page = AppPage;
+
+  navigateToDashboard() {
+    return browser.get('/main/dashboard') as Promise<any>;
+  }
+
+  reload() {
+    browser.sleep(2000);
+    browser.refresh();
+    browser.sleep(2000);
+  }
+}
