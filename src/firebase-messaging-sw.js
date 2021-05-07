5c6530c5ac9e335743ab2d158855fe50071f1ce6
Shiv <treadwill.iitk.2@gmail.com>
2021-05-07 12:43:17 +0530

    notification badge icon added

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,17 +1,29 @@
-importScripts('https://www.gstatic.com/firebasejs/6.3.5/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.3.5/firebase-messaging.js');
-
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
+importScripts(
+  'https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js'
+);
 
 config = {
-  apiKey: "AIzaSyA2uLe3D1HIF3STgFuP7Spp0-UVwL-kK-c",
-  authDomain: "treadwill-81e2f.firebaseapp.com",
-  databaseURL: "https://treadwill-81e2f.firebaseio.com",
-  projectId: "treadwill-81e2f",
-  storageBucket: "treadwill-81e2f.appspot.com",
-  messagingSenderId: "245629293466",
-  appId: "1:245629293466:web:eb0db0c07b608edf"
+  apiKey: 'AIzaSyA2uLe3D1HIF3STgFuP7Spp0-UVwL-kK-c',
+  authDomain: 'treadwill-81e2f.firebaseapp.com',
+  databaseURL: 'https://treadwill-81e2f.firebaseio.com',
+  projectId: 'treadwill-81e2f',
+  storageBucket: 'treadwill-81e2f.appspot.com',
+  messagingSenderId: '245629293466',
+  appId: '1:245629293466:web:eb0db0c07b608edf',
 };
 
 firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
+messaging.onBackgroundMessage(function (payload) {
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: 'assets/icons/icon-144x144.png',
+    badge: 'https://www.treadwill.org/assets/icons/icon.png',
+  };
+  return self.registration.showNotification(
+    payload.data.title,
+    notificationOptions
+  );
+});
