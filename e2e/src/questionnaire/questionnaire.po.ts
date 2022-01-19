6ae491d677722e7f7dcc3f8a8452c676f7cf223f
td_be <lagarwal@iitk.ac.in>
2022-01-19 10:58:16 +0530

    e2e changes

--- e2e/src/questionnaire/questionnaire.po.ts
+++ e2e/src/questionnaire/questionnaire.po.ts
@@ -1,10 +1,13 @@
 import {browser, by, element, protractor} from 'protractor';
 import {FlowPage} from '../flow/flow.po';
+import * as fs from 'fs';
 
 export class QuestionnairePage {
   EC = protractor.ExpectedConditions;
   fp = new FlowPage();
   quesString!: string;
+  quesName!: string;
+
   num!: number;
 
   // navigateTo() {
@@ -27,7 +30,6 @@ export class QuestionnairePage {
   }
 
   clickAnyQuestionnaire() {
-    // element(by.css('mat-card.ques-card.mat-card')).get(2).click();
     const allOptions = element.all(by.css('mat-card.ques-card.mat-card'));
     browser.sleep(2000);
     allOptions
@@ -40,6 +42,19 @@ export class QuestionnairePage {
         allOptions.get(randomNumber).click();
         console.log('Radio button clicked', randomNumber);
       });
+    // browser.wait(this.EC.presenceOf(element(by.css('mat-card.ques-card.mat-card')))).then( () => {
+    //   const questn = element(by.css('mat-card-footer.show-footer-style.mat-card-footer')).element(by.cssContainingText('div.bold-text.giveMeEllipsis', 'Adult ADHD Self-Report Scale (ASRS-v1.1)'));
+    //   browser.sleep(2000);
+    //   questn.click();
+    // });
+  }
+  async getQuestnName() {
+    const el = element(
+      by.css('mat-card-title.title-style.mat-card-title'));
+    browser.wait(this.EC.visibilityOf(el), 1 * 60 * 1000);
+    this.quesName = await el.getText();
+    console.log('ques name', this.quesName);
+   return;
   }
 
   clickBtn(btn: any) {
@@ -56,7 +71,7 @@ export class QuestionnairePage {
   }
   checkPageLoaded() {
     const el = element(by.css('.mat-pre-login-toolbar-color'));
-    browser.wait(this.EC.presenceOf(el)).then(() => {
+    browser.wait(this.EC.presenceOf(el), 1 * 60 * 1000).then(() => {
       console.log('burger button');
       browser.sleep(2000);
       const btn = element(by.className('pre-login-toolbar-burger'));
@@ -94,16 +109,24 @@ export class QuestionnairePage {
     const blank = element(by.css('input'));
     blank.sendKeys('l.goyal18@gmail.com');
     const emailBtn = element(by.cssContainingText('button.mat-raised-button', 'Send'));
-    emailBtn.click();
-    browser.wait(this.EC.invisibilityOf(emailBtn), 1 * 60 * 1000);
+    const emailSendingBtn = element(by.cssContainingText('button.mat-raised-button', 'Sending'));
+    emailBtn.click().then(() => {
+      browser.wait(this.EC.invisibilityOf(emailSendingBtn), 1 * 60 * 1000);
+   });
   }
   clickDownload() {
     const el = element(by.css('a.print-style'));
     browser.wait(this.EC.visibilityOf(el), 1 * 60 * 1000);
     el.click();
+    // browser.wait(this.EC.invisibilityOf(emailBtn), 1 * 60 * 1000);
+    const downloadDirectory = '../Downloads/';
+    const filename = '18-01-22_' + this.quesName + '_Result.pdf';
 
-    const emailBtn = element(by.cssContainingText('button.mat-raised-button', 'Send'));
-    emailBtn.click();
-    browser.wait(this.EC.invisibilityOf(emailBtn), 1 * 60 * 1000);
+    browser.wait(function() {
+      return fs.existsSync(downloadDirectory + filename);
+    }, 1000000).then(function() {
+      expect(filename).toContain('\'18-01-22_\' + this.quesName + \'_Result.pdf\'');
+    });
+
   }
 }
