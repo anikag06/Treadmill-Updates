dc3293b911a7ab74252925ba3993a6b64fcc09ce
latika <Lagarwal@iitk.ac.in>
2024-09-20 05:18:50 +0530

    api endpoint changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   CHAT_HOST: 'wss://bot.treadwill.org:8002',
   CHATBOT_API: 'https://bot.treadwill.org:8002',
-  API_ENDPOINT: 'https://www.api.treadwill.org',
+  API_ENDPOINT: 'https://www.api.treadwill.org:8000',
   firebase: {
     apiKey: 'AIzaSyA2uLe3D1HIF3STgFuP7Spp0-UVwL-kK-c',
     authDomain: 'treadwill-81e2f.firebaseapp.com',
